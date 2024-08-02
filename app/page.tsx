c365deb6c87d73f96640854c4a15d042436a79a8
Vinicius Sousa <135989617+vini2sousa@users.noreply.github.com>
2024-08-02 15:17:50 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,251 @@
-import Image from "next/image";
+'use client';
+
+import React, { useState } from 'react';
+import { Input, Button, Table, Select, message } from 'antd';
+import * as dotenv from 'dotenv';
+dotenv.config();
+const ELEVENLABS_API_KEY = 'sk_9eee905bb8cffa4f9b94b6d584953fa752a2869d76e646a7';
+const { TextArea } = Input;
+const { Option } = Select;
+const columns = [
+  {
+    title: 'Nome da Voz',
+    dataIndex: 'nomeVoz',
+    key: 'nomeVoz',
+  },
+  {
+    title: 'Categoria',
+    dataIndex: 'categoriaVoz',
+    key: 'categoriaVoz',
+  },
+  {
+    title: 'Informações',
+    dataIndex: 'infosVoz',
+    key: 'infosVoz',
+  },
+  {
+    title: 'Preview',
+    dataIndex: 'previewVoz',
+    key: 'previewVoz',
+    render: (text: string) => (
+      <audio controls>
+        <source src={text} type="audio/mpeg"/>
+      </audio>
+    ),
+  },
+];
 
 export default function Home() {
+  const [dataSource, setDataSource] = useState<Array<{
+    nomeVoz: string;
+    categoriaVoz: string;
+    infosVoz: string;
+    previewVoz: string;
+    voiceId: string;
+  }>>([]);
+
+  
+  const [text, setText] = useState<string>(''); 
+  const [audioUrl, setAudioUrl] = useState<string | null>(null); 
+  const [loading, setLoading] = useState(false); 
+  const [selectedVoiceId, setSelectedVoiceId] = useState<string | undefined>(undefined); 
+  const [mostrarTabela, setmostrarTabela] = useState(false);
+
+
+  async function getVozes() {
+    const options = {
+      method: 'GET',
+      headers: { 'xi-api-key': ELEVENLABS_API_KEY },
+    };
+
+    try {
+      const response = await fetch('https://api.elevenlabs.io/v1/voices', options);
+
+      if (!response.ok) {
+        throw new Error('Erro na resposta');
+      }
+
+      const data = await response.json();
+      const listaVozes = data.voices;
+
+      const dadosFormatados = listaVozes.map((voice: any) => {
+        const name = voice.name;
+        const category = voice.category;
+        const labelObject = voice.labels;
+        const labels: string = JSON.stringify(labelObject);
+        const preview_url = voice.preview_url;
+        const voiceId = voice.voice_id;
+
+        return {
+          nomeVoz: name,
+          categoriaVoz: category,
+          infosVoz: labels,
+          previewVoz: preview_url,
+          voiceId, 
+        };
+      });
+
+      setDataSource(dadosFormatados);
+
+    } catch (error) {
+      throw new Error('Erro ao buscar vozes');
+    }
+  }
+
+  const textAreaEvento = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+
+ 
+  async function textToVoice() {
+    if (!text || !selectedVoiceId) 
+      return; 
+
+    setLoading(true);
+
+    try {
+      const audioUrl = await textToAudio(text, selectedVoiceId);
+
+     
+      setAudioUrl(audioUrl);
+      message.success('Áudio gerado com sucesso!');
+    } catch (error) {
+      message.error('Erro ao gerar áudio.');
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  
+  const textToAudio = async (text: string, voiceId: string): Promise<string> => {
+    const url = `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`;
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'xi-api-key': ELEVENLABS_API_KEY,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          text,
+          model_id: 'eleven_turbo_v2_5', 
+        }),
+      });
+
+      if (!response.ok) {
+      
+        throw new Error(`Erro na resposta da API:`);
+      }
+
+      const audioBlob = await response.blob();
+      const audioUrl = URL.createObjectURL(audioBlob);
+      return audioUrl;
+
+    } catch (error) {
+      throw new Error('Erro ao gerar áudio:');
+    }
+  };
+  
+  const visualizacaoTabela = () => {
+    if (!mostrarTabela) {
+      getVozes();
+    }
+    setmostrarTabela(!mostrarTabela);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main style={{
+      minHeight: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      padding: '20px',
+    }}>
+      <div style={{
+        width: '80%',
+        maxWidth: '1000px',
+        textAlign: 'center',
+        backgroundColor: '#fff',
+        padding: '30px',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        borderRadius: '8px',
+      }}>
+        <h1 style={{ fontSize: '2rem', marginBottom: '20px' }}>Aplicação Text-to-Speech</h1> {}
+        <div style={{
+          marginBottom: '20px',
+        }}>
+          <TextArea
+            showCount
+            maxLength={100}
+            onChange={textAreaEvento}
+            placeholder="Insira o texto e teste algumas vozes!"
+            style={{
+              width: '100%',
+              height: 120,
+              resize: 'none',
+            }}
+          />
+          <Button
+            type="primary"
+            size="large"
+            style={{ width: '100%', marginTop: '10px' }}
+            onClick={visualizacaoTabela}
+          >Vozes Disponíveis</Button>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {dataSource.length > 0 && (
+          <>
+            <div style={{ display: 'flex', gap: '10px', marginTop: '10px' }}>
+              <Select
+                placeholder="Selecionar Voz"
+                onChange={(value) => setSelectedVoiceId(value)}
+                style={{ width: '50%' }}
+              >
+                {dataSource.map((voice) => (
+                  <Option key={voice.voiceId} value={voice.voiceId}>
+                    {voice.nomeVoz}
+                  </Option>
+                ))}
+              </Select>
+              <Button
+                type="primary"
+                size="large"
+                style={{ width: '50%' }}
+                onClick={textToVoice}
+                loading={loading}
+                disabled={!text || !selectedVoiceId}
+              >Escutar Áudio</Button>
+            </div>
+          </>
+        )}
+        {audioUrl && (
+          <div style={{ marginTop: '20px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <span style={{ marginRight: '10px' }}>Escute seu texto:</span>
+            <audio controls>
+              <source src={audioUrl} type="audio/mpeg" />
+            </audio>
+          </div>
+        )}
+        {audioUrl && (
+          <Button
+            type="default"
+            style={{ marginTop: '20px' }}
+            onClick={() => window.location.reload()}
+          >Digitar Outro Texto</Button>
+        )}
+        {mostrarTabela && (
+          <div style={{ marginTop: '20px' }}>
+            <Table
+              dataSource={dataSource}
+              columns={columns}
+              rowKey="voiceId"
+              pagination={{
+                pageSize: 4
+              }}
+            />
+          </div>
+        )}
       </div>
     </main>
   );
